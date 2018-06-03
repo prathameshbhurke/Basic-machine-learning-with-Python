f30be4cfd7f2ad604e0479a7707f55e7c470eb31
Prathamesh Bhurke <pbhurke1990@gmail.com>
2018-06-02 18:15:50 -0700

    Added combinations_with_replacement as well

--- Combination_of_strings.py
+++ Combination_of_strings.py
@@ -1,10 +1,14 @@
 from itertools import combinations
+from itertools import combinations_with_replacement
 
 def comb(n1,inp):
     for i in range(1, n1+1):
         for j in sorted(list(combinations(inp, i))):
             print(''.join(j))
 
+def comb2(n1,inp):
+    for j in sorted(list(combinations_with_replacement(inp, n1))):
+        print(''.join(j))
 
 if __name__ == '__main__':
     a = input()
@@ -12,3 +16,5 @@ if __name__ == '__main__':
     inp = sorted(list(a1[0]))
     n1 = int(a1[1])
     comb(n1,inp)
+    print("Now with replacement")
+    comb2(n1,inp)
