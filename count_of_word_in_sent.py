218618dd80e1610fdd668c15c0436382658e1ce9
Prathamesh Bhurke <pbhurke1990@gmail.com>
2018-06-03 02:09:14 -0700

    Update count_of_word_in_sent.py

--- count_of_word_in_sent.py
+++ count_of_word_in_sent.py
@@ -7,7 +7,16 @@ def count_word(inp):
         s.append((x, inp.count(x)))
     print(s)
 
+def count_word_hash(inp):
+    s = {}
+    for x in inp:
+        if x not in s:
+            s[x] = 0
+        s[x] +=1
+    print(s)
+
 if __name__ == '__main__':
     inp_strg = input()
     inp = inp_strg.split()
-    count_word(inp)
+    #count_word(inp)
+    count_word_hash(inp)
